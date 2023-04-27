dd89368ca0d7012488b40d1541d0ccb5cf5130c5
janaiscoding <jana.istrate@gmail.com>
2023-04-27 20:45:32 +0300

    Add some styling to main elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/home.css";
 
 const Home = ({ images }) => {
   const allImgs = images.map((image) => (
     <div className="image-card" key={image.id}>
       <Link to={image.link}>
-        <h1>{image.name}</h1>
         <img src={image.url} alt={image.name} width={500} height={500}></img>
       </Link>
+      <h1 className="image-name">{image.name}</h1>
     </div>
   ));
 
   return (
-    <>
-      <h1> Home Element</h1>
-      <div>{allImgs}</div>
-    </>
+    <div className="home-main">
+      <h1 className="home-title"> Home Element</h1>
+      <div className="card-wrapper">{allImgs}</div>
+    </div>
   );
 };
 
