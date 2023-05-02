6f983c5f0b7bbaacfb587b145dcc329a970cb31a
janaiscoding <jana.istrate@gmail.com>
2023-05-02 20:43:11 +0300

    prepare for firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,11 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAZWbbZwd5_-_epqIoBA3RvaX8ukOBssNY",
   authDomain: "a-photo-tagging-app.firebaseapp.com",
-  databaseURL:
-    "https://a-photo-tagging-app-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://a-photo-tagging-app-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "a-photo-tagging-app",
   storageBucket: "a-photo-tagging-app.appspot.com",
   messagingSenderId: "722627539833",
-  appId: "1:722627539833:web:9ff722cd5ef9d0660fb5a8",
+  appId: "1:722627539833:web:9ff722cd5ef9d0660fb5a8"
 };
 
 const app = initializeApp(firebaseConfig);
