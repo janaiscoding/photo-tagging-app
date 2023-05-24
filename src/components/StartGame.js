dfaf9165fecfe1871eb33321fc304d3cf23649ed
janaiscoding <jana.istrate@gmail.com>
2023-05-24 22:45:29 +0300

    Fix mobile start game

--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -8,12 +8,12 @@ const StartGame = ({ targets, startGame, handleTimer}) => {
     return (
       <motion.div
         key={target.id}
-        className="bg-blue p-3 flex flex-col rounded max-h-fit"
+        className="bg-blue p-3 flex flex-col rounded max-h-fit items-center"
         whileHover={{ scale: 1.02 }}
         layoutId={target.id}
       >
         <a href={target.wikiLink} target="_blank" rel="noreferrer">
-          <img src={target.img} alt={target.name} className="max-h-[150px]" />
+          <img src={target.img} alt={target.name} className="max-h-[90px] md:max-h-[150px]" />
         </a>
         <p className="text-[#fff] text-center">{target.name}</p>
       </motion.div>
@@ -26,12 +26,12 @@ const StartGame = ({ targets, startGame, handleTimer}) => {
     <>
       <div className="min-h-[90vh] bg-pink p-3 flex items-center justify-center">
         <motion.div
-          className="flex flex-col justify-center bg-soft-pink border border-solid border-grey p-10 rounded drop-shadow-md gap-5 items-center"
+          className="flex flex-col justify-center bg-soft-pink border border-solid border-grey p-3 md:p-10 rounded drop-shadow-md gap-5 items-center"
           initial={{ opacity: 0, scale: 0.7 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <p className="text-center text-3xl">Scroll vertically and horizontally in the image.</p>
+          <p className="text-center text-xl md:text-3xl">Scroll vertically and horizontally in the image.</p>
           <p>Find them as fast as you can!</p>
           <div className="flex gap-3 w-fit"> {targetsList}</div>
           <div className="flex gap-3">
