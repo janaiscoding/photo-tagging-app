8a538060825fdde8bfc016f85eb5dad8fed4638d
janaiscoding <jana.istrate@gmail.com>
2023-05-24 22:50:01 +0300

    Add source to image in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,20 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="min-h-[5vh] flex items-center justify-center">
+      <footer className="min-h-[5vh] flex items-center gap-5 justify-center">
         <a
           href="https://github.com/janaiscoding"
           className="text-pink"
           rel="noopener noreferrer"
         >
           ©JanaIsCoding
+        </a>
+        <a
+          href="https://www.lewiscarroll.org/2012/07/06/discussing-the-divine-comedy-with-dante-and-lewis-carroll/"
+          className="text-pink"
+          rel="noopener noreferrer"
+        >
+          Image Source
         </a>
       </footer>
     </>
